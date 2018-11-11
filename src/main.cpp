68c758fe7b80712bdc762f2482f034d624d314d7
Valentin Nasraty <valentin.nasraty@epitech.eu>
2018-11-11 11:07:15 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
 #pragma comment(lib, "crypt32.lib")
 #pragma comment(lib, "sqlite3.lib")
 
-#include <iostream>
-#include <string>
-#include <Windows.h>
-#include <ShlObj.h>
-
-# define C_DBFILE "\\Google\\Chrome\\User Data\\Default\\Login Data" 
-# define C_REGKEY L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\chrome.exe"
-
-# define O_DBFILE "\\Opera Software\\Opera Stable\\Login Data" 
-# define O_REGKEY L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\opera.exe"  
+#include "BrowserPassword.hh"
 
 int main(int ac, const char **av) 
 {
