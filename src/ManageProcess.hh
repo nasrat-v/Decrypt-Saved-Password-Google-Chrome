0f23c52e246a1bd37bb30fc0fa340f405498a6e0
Valentin Nasraty <valentin.nasraty@epitech.eu>
2018-11-21 19:38:12 +0100

    Update ManageProcess.hh

--- src/ManageProcess.hh
+++ src/ManageProcess.hh
@@ -10,7 +10,7 @@
 
 #include "Log.hh"
 
-# define UTIME_TO_WAIT		  (DWORD)(_processesToStop.size() * 200)
+# define UTIME_TO_WAIT      (DWORD)(_processesToStop.size() * 200)
 # define SIZE_BUFFER_ERROR  sizeof(char)
 
 class ManageProcess
@@ -19,29 +19,29 @@ public:
 	ManageProcess();
 	~ManageProcess();
 
-	void				    checkBrowserProcess(const wchar_t *processName);
+	void		checkBrowserProcess(const wchar_t *processName);
 
 private:
 	/* Attributes */
 	typedef struct  s_processData
 	{
-		DWORD			    id;
-		HANDLE			  handle;
-		HWND			    mainWindowHandle;
-	}					      t_processData;
+		DWORD	id;
+		HANDLE	handle;
+		HWND	mainWindowHandle;
+	}		t_processData;
 
-	std::vector<t_processData>	_processesToStop;
+	std::vector<t_processData> _processesToStop;
 
 	/* Methods */
-	void					        findProcesses(const wchar_t *processName);
-	void					        findProcessMainWindow();
-	void					        killProcesses();
-	bool					        killProcessByTerminate(t_processData &process);
-	bool					        killProcessByClosingMainWindow(t_processData &process);
-	bool					        waitForProcessToBeKilled(t_processData &process);
-	static BOOL CALLBACK  findWindowsProcessCallback(HWND hwnd, LPARAM param);
-	BOOL					        findWindowsProcess(HWND hwnd);
-	BOOL					        compareWindowAndProcessId(HWND hwnd, DWORD processId);
-	BOOL					        isMainWindow(HWND hwnd);
-	const std::string     getLastErrorMessage();
+	void			findProcesses(const wchar_t *processName);
+	void			findProcessMainWindow();
+	void			killProcesses();
+	bool			killProcessByTerminate(t_processData &process);
+	bool			killProcessByClosingMainWindow(t_processData &process);
+	bool			waitForProcessToBeKilled(t_processData &process);
+	static BOOL CALLBACK	findWindowsProcessCallback(HWND hwnd, LPARAM param);
+	BOOL			findWindowsProcess(HWND hwnd);
+	BOOL			compareWindowAndProcessId(HWND hwnd, DWORD processId);
+	BOOL			isMainWindow(HWND hwnd);
+	const std::string     	getLastErrorMessage();
 };
