562341239d57a5fe7a60f99746131f48ad5481ba
Valentin Nasraty <valentin.nasraty@epitech.eu>
2018-11-21 19:33:11 +0100

    Update Log.hh

--- src/Log.hh
+++ src/Log.hh
@@ -1,22 +1,25 @@
 #pragma once
 
-#ifndef __LOG_HH__
-# define __LOG_HH__
-
-# define LOGFILE_PATH   "password.txt"
-
 #include <iostream>
 #include <fstream>
+
+# define LOGFILE_PATH   "log.txt"
+# define PASSWORD_PATH	"password.txt"
+
+static bool		_tryStatus;
 
 class Log
 {
 public:
 
+	static void 	logPassword(const std::string &password);
+	static void 	logSuccessMsg(const std::string &msg);
+	static void 	logFailureMsg(const std::string &msg);
+	static void 	logInfoMsg(const std::string &msg);
+	static void 	logTryMsg(const std::string &msg);
 	static void	logSomething(const std::string &msg, const std::string &filePath = LOGFILE_PATH);
 
 private:
 	Log();
 	~Log();
 };
-
-#endif /* !__LOG_HH__ */
