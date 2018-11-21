32464df99acd7a09eae5428e57b55d588c9e3d3b
Valentin Nasraty <valentin.nasraty@epitech.eu>
2018-11-21 19:32:29 +0100

    Update Log.cpp

--- src/Log.cpp
+++ src/Log.cpp
@@ -1,7 +1,10 @@
 
 #include "Log.hh"
 
-Log::Log() = default;
+Log::Log()
+{
+	_tryStatus = false;
+}
 
 Log::~Log() = default;
 
@@ -11,10 +14,53 @@ Log::~Log() = default;
  * @param complementary_msg Additional message to display
  * @param file_path Special filepath, if empty the default file path is the macro LOGFILE_PATH
  */
-void                Log::logSomething(const std::string &msg, const std::string &filePath)
+ void Log::logSomething(const std::string &msg, const std::string &filePath)
 {
-	std::fstream    file(filePath, std::fstream::out | std::fstream::app);
+	std::fstream file(filePath, std::fstream::out | std::fstream::app);
 
 	file << msg.c_str() << std::endl;
 	file.close();
 }
+
+ /**
+ * Static function, can be called everywhere.
+ * Log password in password log file
+ * @param password Password infos to display
+ */
+ void Log::logPassword(const std::string &password)
+ {
+	 logSomething(password, PASSWORD_PATH);
+ }
+
+ void Log::logSuccessMsg(const std::string &msg)
+ {
+	 if (_tryStatus)
+	 {
+		 logSomething("\t\t[OK] " + msg);
+		 _tryStatus = false;
+	 }
+	 else
+		 logSomething("[OK] " + msg);
+ }
+
+ void Log::logFailureMsg(const std::string &msg)
+ {
+	 if (_tryStatus)
+	 {
+		 logSomething("\t\t[KO] " + msg);
+		 _tryStatus = false;
+	 }
+	 else
+		 logSomething("[KO] " + msg);
+ }
+
+ void Log::logInfoMsg(const std::string &msg)
+ {
+	 logSomething("\n[info] " + msg);
+ }
+
+ void Log::logTryMsg(const std::string &msg)
+ {
+	 logSomething("\t[try] " + msg);
+	 _tryStatus = true;
+ }
